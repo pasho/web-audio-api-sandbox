c10fb947e09c49e07285e227106349a4f0661d08
Pasho <pfedulov84@gmail.com>
2021-07-14 22:26:19 +0100

    Simple file read

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const readFile = (file: File) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      resolve(reader.result as string);
+    }
+    reader.onerror = () => {
+      reject('Failed to read');
+    }
+
+    reader.readAsText(file);
+  })
+}
+
 function App() {
+  const [file, setFile] = useState<File | undefined>();
+  const [content, setContent] = useState<string>('');
+  const fileRef = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+
+    const loadFile = async () => {
+      try {
+        const fileContent = await readFile(file);
+        setContent(fileContent);
+      }
+      catch (e) {
+        console.error(e);
+      }
+    }
+
+    loadFile();
+  }, [file]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* @ts-ignore */}
+      <input type="file" ref={fileRef} onChange={(e) => {
+        console.log(e.target.files)
+        setFile((e.target.files ?? [])[0]);
+      }} />
+      <div>{content}</div>
     </div>
   );
 }
