a0f958908d8bfa42ad24af2162dc34950ca0ff71
Pasho <pfedulov84@gmail.com>
2021-07-19 21:50:33 +0100

    Visualization of the sound wave from a file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-const readFile = (file: File) => {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      resolve(reader.result as string);
-    }
-    reader.onerror = () => {
-      reject('Failed to read');
-    }
-
-    reader.readAsText(file);
-  })
-}
+const WIDTH = 500;
+const HEIGHT = 100;
 
 function App() {
   const [file, setFile] = useState<File | undefined>();
   const [content, setContent] = useState<string>('');
   const fileRef = useRef<HTMLInputElement>();
+  const canvasRef = useRef<HTMLCanvasElement>();
 
   useEffect(() => {
     if (!file) {
@@ -26,13 +16,61 @@ function App() {
     }
 
     const loadFile = async () => {
-      try {
-        const fileContent = await readFile(file);
-        setContent(fileContent);
-      }
-      catch (e) {
-        console.error(e);
-      }
+      const audio = await file.arrayBuffer();
+
+      const audioCtx = new AudioContext();
+      const analyser = audioCtx.createAnalyser();
+
+      const decoded = await audioCtx.decodeAudioData(audio);
+      const source = audioCtx.createBufferSource();
+      source.buffer = decoded;
+
+      source.connect(analyser);
+      analyser.connect(audioCtx.destination);
+      source.start();
+
+      analyser.fftSize = 2048;
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      const canvasCtx = canvasRef.current?.getContext("2d")!;
+      canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+      function draw() {
+        var drawVisual = requestAnimationFrame(draw);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+        canvasCtx.beginPath();
+
+        var sliceWidth = WIDTH * 1.0 / bufferLength;
+        var x = 0;
+
+        for (var i = 0; i < bufferLength; i++) {
+
+          var v = dataArray[i] / 128.0;
+          var y = v * HEIGHT / 2;
+
+          if (i === 0) {
+            canvasCtx.moveTo(x, y);
+          } else {
+            canvasCtx.lineTo(x, y);
+          }
+
+          x += sliceWidth;
+        }
+
+        canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+        canvasCtx.stroke();
+      };
+
+      draw();
     }
 
     loadFile();
@@ -45,7 +83,8 @@ function App() {
         console.log(e.target.files)
         setFile((e.target.files ?? [])[0]);
       }} />
-      <div>{content}</div>
+      {/* @ts-ignore */}
+      <canvas ref={canvasRef} width={WIDTH} height={HEIGHT}></canvas>
     </div>
   );
 }
